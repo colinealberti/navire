3ad593bc8ab05171b7c1cdac6dbeee919e121044
Coline ALBERTI <coline.alberti@gmail.com>
2020-12-09 14:55:18 +0100

    formulaire message, controller, template

--- src/Entity/Message.php
+++ src/Entity/Message.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\MessageRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=MessageRepository::class)
