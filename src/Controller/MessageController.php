b06f3995318f9a209113f3d1cc59b53b2870b27b
Coline ALBERTI <coline.alberti@gmail.com>
2020-12-09 16:53:11 +0100

    maj du lien dans le footer

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Swoole\Http\Request;
+use Symfony\Component\HttpFoundation\Request;
 use GestionContact;
 use App\Entity\Message;
 
