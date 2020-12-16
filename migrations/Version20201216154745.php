f9e81079c8f2c136d9338c30561f7b6656dc96db
Coline ALBERTI <coline.alberti@gmail.com>
2020-12-16 17:26:36 +0100

    Entity AisShipType make:migration

--- migrations/Version20201216154745.php
+++ migrations/Version20201216154745.php
@@ -20,11 +20,11 @@ final class Version20201216154745 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE navire DROP FOREIGN KEY FK_EED1038E62DB837');
-        $this->addSql('CREATE TABLE aisshiptype (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(60) NOT NULL, ais_ship_type INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        //$this->addSql('ALTER TABLE navire DROP FOREIGN KEY FK_EED1038E62DB837');
+        //$this->addSql('CREATE TABLE aisshiptype (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(60) NOT NULL, ais_ship_type INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('DROP TABLE ais_ship_type');
-        $this->addSql('ALTER TABLE navire DROP FOREIGN KEY FK_EED1038E62DB837');
-        $this->addSql('ALTER TABLE navire ADD CONSTRAINT FK_EED1038E62DB837 FOREIGN KEY (idAisShipType) REFERENCES ais_ship_type(id)');
+        //$this->addSql('ALTER TABLE navire DROP FOREIGN KEY FK_EED1038E62DB837');
+        $this->addSql('ALTER TABLE navire ADD CONSTRAINT FK_EED1038E62DB837 FOREIGN KEY (id) REFERENCES ais_ship_type(id)');
     }
 
     public function down(Schema $schema) : void
